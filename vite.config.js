533f4989371d1b222e651e2e940c81ffcfdb5a35
da <dain940209@gmail.com>
2024-02-29 23:49:40 +0900

    fix : deploy

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,17 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   plugins: [],
   build: {
+    outDir: 'dist',       // 빌드된 파일이 저장될 디렉토리
+    assetsDir: '',  // 정적 파일이 저장될 디렉토리
     rollupOptions: {
-      input: './src/main.js', 
+      input: [
+        './index.html',
+        './src/main.js',
+      ], 
+      // entryFileNames: 'main.js',
+      // chunkFileNames: 'main.js', 
     },
+    //assetsInclude: ['index.html','src','public'],
   },
-  base:""
+  base:"/standard_social_game_client/",
 });
